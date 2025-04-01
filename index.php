28358af9f3a970aed54e8463df4e02ad70528657
Xenor <144612346+DelsolTiberian@users.noreply.github.com>
2025-04-01 14:13:20 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+    session_start();
+?>
+
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Medisur Tickets</title>
+    <link rel="stylesheet" href="style/main.css">
+    <!-- Imports -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+<body class="medisur-gradient center">
+    <div class="log-bg center">
+        <h1> Log-In</h1>
+        <form class="center" action="index.php" method="post">
+            <input class="log-input" type="text" placeholder="Identifiant" name="id"/>
+            <input class="log-input" type="password" placeholder="Mot De Passe" name="password"/>
+            <input class="submit" type="submit" value="Connexion" />
+
+            <?php
+
+
+            ?>
+
+        </form>
+    </div>
+</body>
+</html>

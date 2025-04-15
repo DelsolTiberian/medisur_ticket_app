8e18ac23147abf6493b0005cc520a948f72ec307
tiberian <td@tiberiandelsol.com>
2025-04-15 12:32:04 +0200

    add tailwind

--- index.php
+++ index.php
@@ -9,33 +9,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Medisur Tickets</title>
-    <link rel="stylesheet" href="style/main.css">
     <!-- Imports -->
     <link href="src/output.css" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 <body >
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-</h1>
-    <div >
-        <h1> Log-In</h1>
-        <form action="index.php" method="post">
-            <!--suppress HtmlFormInputWithoutLabel -->
-            <input class="" type="text" placeholder="Identifiant" name="user_id"/>
-            <!--suppress HtmlFormInputWithoutLabel -->
-            <input class="" type="password" placeholder="Mot De Passe" name="password"/>
-            <input class="" type="submit" value="connexion" />
-
-            <?php
-                // Include important sql requesting functiuns
-                include 'php_assets/database_command.php';
-
-            ?>
-
-        </form>
+    <div class="bg-gradient-to-br from-purple-700 to-pink-500 min-h-screen flex flex-col justify-center items-center">
+        <div class="bg-white rounded-lg shadow-lg p-8 max-w-md">
+            <h1 class="text-4xl font-bold text-center text-purple-700 mb-8">Welcome to My Website</h1>
+            <form class="space-y-6">
+                <div>
+                    <label class="block text-gray-700 font-bold mb-2" for="email">
+                        Email
+                    </label>
+                    <input class="w-full px-4 py-2 rounded-lg border border-gray-400" id="email" name="email"
+                           type="email">
+                </div>
+                <div>
+                    <label class="block text-gray-700 font-bold mb-2" for="password">
+                        Password
+                    </label>
+                    <input class="w-full px-4 py-2 rounded-lg border border-gray-400" id="password" name="password"
+                           type="password">
+                </div>
+                <div>
+                    <button class="w-full bg-purple-700 hover:bg-purple-900 text-white font-bold py-2 px-4 rounded-lg">
+                        Log In
+                    </button>
+                </div>
+            </form>
+        </div>
     </div>
 </body>
 </html>

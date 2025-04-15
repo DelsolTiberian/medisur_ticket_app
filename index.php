dac8031f9e04e41f005f446a3960b67a692fea86
tiberian <td@tiberiandelsol.com>
2025-04-15 11:03:00 +0200

    add tailwindcss

--- index.php
+++ index.php
@@ -11,20 +11,23 @@
     <title>Medisur Tickets</title>
     <link rel="stylesheet" href="style/main.css">
     <!-- Imports -->
-    <link href="./output.css" rel="stylesheet">
+    <link href="src/output.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
-<body class="medisur-gradient center">
-    <div class="log-bg center">
+<body >
+<h1 class="text-3xl font-bold underline">
+    Hello world!
+</h1>
+    <div >
         <h1> Log-In</h1>
-        <form class="center" action="index.php" method="post">
+        <form action="index.php" method="post">
             <!--suppress HtmlFormInputWithoutLabel -->
-            <input class="log-input" type="text" placeholder="Identifiant" name="user_id"/>
+            <input class="" type="text" placeholder="Identifiant" name="user_id"/>
             <!--suppress HtmlFormInputWithoutLabel -->
-            <input class="log-input" type="password" placeholder="Mot De Passe" name="password"/>
-            <input class="log-input" type="submit" value="connexion" />
+            <input class="" type="password" placeholder="Mot De Passe" name="password"/>
+            <input class="" type="submit" value="connexion" />
 
             <?php
                 // Include important sql requesting functiuns

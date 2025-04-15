be3711f73128824b4ca814beee4f1900d83793cf
tiberian <td@tiberiandelsol.com>
2025-04-15 10:51:21 +0200

    add tailwindcss

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
     <title>Medisur Tickets</title>
     <link rel="stylesheet" href="style/main.css">
     <!-- Imports -->
+    <link href="./output.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
@@ -19,7 +20,9 @@
     <div class="log-bg center">
         <h1> Log-In</h1>
         <form class="center" action="index.php" method="post">
+            <!--suppress HtmlFormInputWithoutLabel -->
             <input class="log-input" type="text" placeholder="Identifiant" name="user_id"/>
+            <!--suppress HtmlFormInputWithoutLabel -->
             <input class="log-input" type="password" placeholder="Mot De Passe" name="password"/>
             <input class="log-input" type="submit" value="connexion" />
 
@@ -27,19 +30,6 @@
                 // Include important sql requesting functiuns
                 include 'php_assets/database_command.php';
 
-                $identifier = explode("_", $_POST['user_id']) ?? NULL; //user's identifer is composed with user's lastname + "_" +  user's firstname
-                $password = $_POST['password'] ?? NULL;
-                if(isset($identifier) && isset($password)){
-                    // Ask if user exist and what is his role
-                    $user = db_select("SELECT role.name, user.id, user.profile_picture_url FROM user JOIN role ON user.role = role.id WHERE user.last_name LIKE '$identifier[0]' AND user.first_name LIKE '$identifier[1]' AND password LIKE '$password';") ?? NULL;
-                    $_SESSION["role"] = $user['name'];
-                    // If the user exist: redirect on his home page
-                    if (isset($_SESSION["role"])){
-                        $_SESSION["user_id"] = $user['id']; // Only unic identifier that is independant of his first and last names
-                        $_SESSION["user_photo"] = $user['profile_picture_url'] ?? NULL;
-                        header("Location: ./pages/user_main.php");
-                    }
-                }
             ?>
 
         </form>

92067562abe58fd5d63e9890d55be48c46b85e3c
tiberian <td@tiberiandelsol.com>
2025-04-22 11:17:05 +0200

    suppr php

--- index.php
+++ index.php
@@ -23,24 +23,7 @@
             <input class="log-input" type="password" placeholder="Mot De Passe" name="password"/>
             <input class="log-input" type="submit" value="connexion" />
 
-            <?php
-                // Include important sql requesting functiuns
-                include 'php_assets/database_command.php';
 
-                $identifier = explode("_", $_POST['user_id']) ?? NULL; //user's identifer is composed with user's lastname + "_" +  user's firstname
-                $password = $_POST['password'] ?? NULL;
-                if(isset($identifier) && isset($password)){
-                    // Ask if user exist and what is his role
-                    $user = db_select("SELECT role.name, user.id, user.profile_picture_url FROM user JOIN role ON user.role = role.id WHERE user.last_name LIKE '$identifier[0]' AND user.first_name LIKE '$identifier[1]' AND password LIKE '$password';") ?? NULL;
-                    $_SESSION["role"] = $user['name'];
-                    // If the user exist: redirect on his home page
-                    if (isset($_SESSION["role"])){
-                        $_SESSION["user_id"] = $user['id']; // Only unic identifier that is independant of his first and last names
-                        $_SESSION["user_photo"] = $user['profile_picture_url'] ?? NULL;
-                        header("Location: ./pages/user_main.php");
-                    }
-                }
-            ?>
 
         </form>
     </div>
